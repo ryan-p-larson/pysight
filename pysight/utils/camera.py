6c4b3630ea0ce8c1f3b1895903cef69df6dae255
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-01-31 11:54:50 -0600

    Change camera processing() methods. Added OpenCV interpolaters for resizing.

--- pysight/utils/camera.py
+++ pysight/utils/camera.py
@@ -3,7 +3,7 @@ PySight - Camera class
 """
 
 # OpenCV resize docs mention INTER_AREA is best for shrinking images, linear is fater
-from cv2 import VideoCapture, COLOR_BGR2GRAY, equalizeHist, cvtColor, resize, INTER_LINEAR, INTER_AREA
+from cv2 import VideoCapture, COLOR_RGB2GRAY, equalizeHist, cvtColor, resize, INTER_LINEAR, INTER_AREA
 
 class Camera(object):
 	"""Helper class that eases the setup/teardown of an OpenCV webcam."""
@@ -15,7 +15,7 @@ class Camera(object):
 	def capture(self):
 		"""Function to start, capture, and shutdown our camera."""
 		
-		webcam = VideoCapture(self.__src) # Fire up our webcam
+		webcam = VideoCapture(self.__src) 	# Fire up our webcam
 		grabbed, frame = webcam.read()      # Grab an image as uint8 array.
 		webcam.release()                    # Stop our webcam so we can use it again.
 		
@@ -31,11 +31,11 @@ class Camera(object):
 
 		# Down sample image so our algo has to do less work.
 		# we need to keep in mind aspect ratio so the image doesn't look distorted.
-		img_width, img_height = img.shape[0], img.shape[1]
+		img_width, img_height = img.shape[1], img.shape[0]
 		down_width, down_height = int(img_width / downsize), int(img_height / downsize)
 		dim = (down_width, down_height)
  
 		img_scaled		= resize(img, dim, interpolation=INTER_AREA)# Resize with our calc'd
-		img_gray 		= cvtColor(img_scaled, COLOR_BGR2GRAY) 		# Convert to grayscale.
+		img_gray 		= cvtColor(img_scaled, COLOR_RGB2GRAY) 		# Convert to grayscale.
 		img_equalized 	= equalizeHist(img_gray)					# Equalize the values
 		return img_equalized
