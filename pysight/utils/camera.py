ac14de9fb8689a8d28870d39ed8cb24f5b04a7a8
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-01-30 20:25:45 -0600

    Added a 'downsize' parameter to the camera processing method.

--- pysight/utils/camera.py
+++ pysight/utils/camera.py
@@ -2,7 +2,8 @@
 PySight - Camera class
 """
 
-# from cv2 import ...
+# OpenCV resize docs mention INTER_AREA is best for shrinking images, linear is fater
+from cv2 import VideoCapture, COLOR_BGR2GRAY, equalizeHist, cvtColor, resize, INTER_LINEAR, INTER_AREA
 
 class Camera(object):
 	"""Helper class that eases the setup/teardown of an OpenCV webcam."""
@@ -14,16 +15,27 @@ class Camera(object):
 	def capture(self):
 		"""Function to start, capture, and shutdown our camera."""
 		
-		webcam = cv2.VideoCapture(self.__src) # Fire up our webcam
+		webcam = VideoCapture(self.__src) # Fire up our webcam
 		grabbed, frame = webcam.read()      # Grab an image as uint8 array.
 		webcam.release()                    # Stop our webcam so we can use it again.
 		
 		return frame
 
-	def process(self, img):
-		"""Function to process an RGB image into an B&W, equalized img."""
+	def process(self, img, downsize=5):
+		"""Function to process an raw RGB image into an B&W, equalized img.
+		Args:
+			img (np.array): Image in the form of a np.nd? array, typically from self.poll()
+		Returns:
+			img_equalized (np.ndarray): Processed image.
+		"""
 
-		# resize image
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 	# Convert to grayscale.
-		equalized = cv2.equalizeHist(gray)				# Equalize the values
-		return equalized
+		# Down sample image so our algo has to do less work.
+		# we need to keep in mind aspect ratio so the image doesn't look distorted.
+		img_width, img_height = img.shape[0], img.shape[1]
+		down_width, down_height = int(img_width / downsize), int(img_height / downsize)
+		dim = (down_width, down_height)
+ 
+		img_scaled		= resize(img, dim, interpolation=INTER_AREA)# Resize with our calc'd
+		img_gray 		= cvtColor(img_scaled, COLOR_BGR2GRAY) 		# Convert to grayscale.
+		img_equalized 	= equalizeHist(img_gray)					# Equalize the values
+		return img_equalized
