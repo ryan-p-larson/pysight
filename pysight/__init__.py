94774b31ceede0065d346cbce3b4e5ac5f55fcb4
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-01-31 12:01:48 -0600

    Add face.py: a collection of Computer Vision predictors.

--- pysight/__init__.py
+++ pysight/__init__.py
@@ -6,5 +6,6 @@ Landmarks - Haar Caascades, Facial Landmarks, and computer vision.
 Gaze - Functions for getting haze estimation.
 """
 
+from pysight.face import Face
 from pysight import utils
 from pysight.utils.camera import Camera
