eb59aab1688320eb9fbf7924b06af0550ebe639c
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-01-30 18:30:59 -0600

    Add numeric constants to utilities module.

--- pysight/utils/__init__.py
+++ pysight/utils/__init__.py
@@ -2,6 +2,8 @@
 PySight - Utilities
 
 Camera - Helper Camera class that eases setting up/capturing, and tearing down an OpenCV webcam.
+Numeric Constants - Pulled from DLIB/BOOST.
 """
 
 from . import camera
+from . import numeric_constants
---
+++ pysight/utils/numeric_constants.py
@@ -0,0 +1,46 @@
+"""
+Numeric Constants
+Sourced from Dlib/Boost software.
+"""
+
+# pi -- Pi
+pi = 3.1415926535897932385
+
+# e  -- Euler's Constant
+e = 2.7182818284590452354
+
+# sqrt_2 -- The square root of 2
+sqrt_2 = 1.4142135623730950488
+
+# sqrt_3 -- The square root of 3
+sqrt_3 = 1.7320508075688772935
+
+# log10_2 -- The logarithm base 10 of two
+log10_2 = 0.30102999566398119521
+
+# light_spd -- The speed of light in vacuum in meters per second
+light_spd = 2.99792458e8
+
+# newton_G  -- Newton's gravitational constant (in metric units of m^3/(kg*s^2))
+newton_G = 6.67384e-11
+
+# planck_cst -- Planck's constant (in units of Joules * seconds) 
+planck_cst = 6.62606957e-34
+
+# golden_ratio -- The Golden Ratio
+golden_ratio = 1.6180339887498948482
+
+# euler_gamma -- The Euler Mascheroni Constant 
+euler_gamma = 0.5772156649015328606065
+
+# catalan -- Catalan's Constant
+catalan = 0.91596559417721901505 
+
+# glaisher -- Glaisher Kinkelin constant
+glaisher = 1.2824271291006226369
+
+# khinchin -- Khinchin's constant
+khinchin = 2.6854520010653064453
+
+# apery -- Apery's constant
+apery = 1.2020569031595942854
